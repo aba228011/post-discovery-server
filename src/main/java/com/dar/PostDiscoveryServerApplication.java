0ccf298828b415e39b105a63a1b46be7d5ef7ba3
abylai <a.zhalgasbai@csi.kz>
2022-03-24 12:27:35 +0600

    first commit

---
+++ src/main/java/com/dar/PostDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.dar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostDiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostDiscoveryServerApplication.class, args);
+    }
+
+}
